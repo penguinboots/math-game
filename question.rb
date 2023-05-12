2b15f3975c0b452a281fe3defad30c2d063b97f5
penguinboots <sabrina.ynw@gmail.com>
2023-05-12 14:41:17 -0700

    add Question class

---
+++ question.rb
@@ -0,0 +1,14 @@
+class Question
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @sum = @num1 + @num2
+  end
+
+  def ask(player)
+    puts "#{player}: What is #{@num1} plus #{@num2}?"
+  end
+
+  def check(input)
+    @sum == input
+  end
